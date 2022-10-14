785cbfea395dcbe33afb47f8c7a460914304368a
Joabe Lopes <104812737+joabeslopes@users.noreply.github.com>
2022-10-14 01:33:31 -0300

    Update ordenaVetor.py

--- Estruturas de repetição e condicionais/ordenaVetor.py
+++ Estruturas de repetição e condicionais/ordenaVetor.py
@@ -6,13 +6,13 @@ for i in range(len(numero)-1):
 # A variavel 'i' só precisa ir até o penúltimo valor, pois aí será comparada com o último 
     for j in range(i+1, len(numero)):
 
-# é preciso converter os valores da lista para números
+# É preciso converter os valores da lista para números
         numero[i] = int(numero[i])
         numero[j] = int(numero[j])
         
         if numero[i] > numero[j]:
 
-# a variavel 'maior' serve apenas para fazer a troca dos valores
+# A variavel 'maior' serve apenas para fazer a troca dos valores
             maior = numero[i]
             numero[i] = numero[j]
             numero[j] = maior
